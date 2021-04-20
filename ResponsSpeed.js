fe1314e0c153cee3fe87b74a0144b77f47f0171e
Citizen <40601526+ViGilanteAF@users.noreply.github.com>
2021-04-21 02:48:55 +0900

    Resons time

--- ResponsSpeed.js
+++ ResponsSpeed.js
@@ -3,22 +3,42 @@ var 스크린 = document.querySelector('#screen');
 var state = {
 
 };
+var startTime;
+var finalTime;
+//스코프 를 이용하여 호출스택에서 나가서 시작시간이 계속 저장이 되게끔 만들어둠
+var recoDe=[];
+// console.time('시간');
+// console.timeEnd('시간');
+// 보통 디버깅시 에 많이 사용함
+
+// var 시작시간 = performance.now();
+// var 끝시간 = performance.now();
+//    console.log((끝시간 - 시작시간)/1000);
+//정밀한 시간을 측정하고 싶을경우 사용함
+
+//new Date(); 를 하는 순간 그 시각이 저장이 됨
+
 스크린.addEventListener('click',function(){
-    if(스크린.classList.contains('waiting')){
+    if(스크린.classList.contains('waiting')){ //대기
         //classList.contains로 현재 클래스를 파악할 수 있음
         스크린.classList.remove('waiting');
         스크린.classList.add('ready');
         스크린.textContent = '초록색이 되면 클릭하세요!';
-    }else if(스크린.classList.contains('ready')){
+        setTimeout(function() {
+            startTime  = new Date();
+            스크린.click();
+        }, Math.floor(Math.random() * 1000)+ 2000);
+    }else if(스크린.classList.contains('ready')){ //준비
         스크린.classList.remove('ready');
         스크린.classList.add('now');
         스크린.textContent = '클릭하세요!';
-    }else if(스크린.classList.contains('now')){
+    }else if(스크린.classList.contains('now')){ //시작
+        finishTime = new Date();
+        console.log('반응속도',finishTime - startTime,'ms')
         스크린.classList.remove('now');
         스크린.classList.add('waiting');
         스크린.textContent = '클릭해서 시작하세요!';
     }
-    
     
     
 });
